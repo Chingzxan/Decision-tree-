505733974cd43fc6eec4e31b1865e44f3d7985ca
Chingizxan <danil.shadrin05092007@gmail.com>
2026-05-18 22:47:39 +0700

    Для работы с датасетом делаем структуру,где будут находиться значения

--- tree.h
+++ tree.h
@@ -1,1 +1,12 @@
+#ifndef Tree_H
+#define Tree_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct datas{
+    int kol_prim; //количество примеров в датасте
+    int kol_priz; //количество признаков
+    float **data; // признаки
+    int *prav; // правильный вариант
+}Datas
