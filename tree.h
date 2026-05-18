cd6b5ad251a4ac82345457eb87455356f56209d5
Chingizxan <danil.shadrin05092007@gmail.com>
2026-05-18 23:28:17 +0700

    Структуры для дерева и его разбиения

--- tree.h
+++ tree.h
@@ -9,4 +9,21 @@ typedef struct datas{
     int kol_priz; //количество признаков
     float **data; // признаки
     int *prav; // правильный вариант
-}Datas
+}Datas;
+
+typedef struct Node //куда что будет относиться
+{
+    int uzel;
+    int predict_class;
+    int index_priznak;
+    float znach;// условия для разбиения
+    struct Node *left;
+    struct Node *right;
+}Node;
+
+typedef struct Tree{
+    Node *koren;
+    int max_glub;//глубина
+    int min_split;//количество признаков или данных для разбиения
+    int kol_priz;// рандомное количество признаков 
+}
