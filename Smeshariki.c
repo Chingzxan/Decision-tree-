ba2fd45deccb842e2df0f0c8b3208cb1b26d064a
Chingizxan <danil.shadrin05092007@gmail.com>
2026-05-23 15:57:41 +0700

    Начало главного файла:прочитка что закинули

--- Smeshariki.c
+++ Smeshariki.c
@@ -1,1 +1,18 @@
+#include "beging.h"
 
+int main(int argc,char*argv[])
+{
+    if(argc>=2 && argc<=5)
+    {
+        printf("Vasab, vveditolko: name dataseta,kolichestvo tree
+            ,glub tree,minimum split, kolichestvo priznakov.Udachi🐉");
+
+        return 1;
+    }
+    
+    char *datafile =argv[1];
+
+    int kol_tree =atoi(argv[2]);
+
+
+}
