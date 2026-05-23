71485deee23b1f0b5f8147888f79b4c56f1271c3
Chingizxan <danil.shadrin05092007@gmail.com>
2026-05-23 14:38:44 +0700

    Создание бег модели и различные разбиения для бегинга

--- beging.c
+++ beging.c
@@ -1,1 +1,30 @@
+#include "beging.h"
 
+static Datas* bootstrap_sample(Datas* ds)
+{
+    Datas* sample=create_datas(ds->kol_prim,ds->kol_priz);
+    for(int i=0;i<ds->kol_prim;i++)
+    {
+        int ind=rand()%ds->kol_prim;
+        for(int g=0;g<ds->kol_priz;g++)
+        {
+            sample->data[i][g]=ds->data[ind][f];
+        }
+        sample->prav[i]=ds->prav[ind];
+    }
+    return sample;
+
+}
+BegModel* create_beg_model(int kol_tree,int max_glub,int min_split,int kol_priz_sub)
+{
+    BegModel* mod=malloc(sizeof(BegModel));
+
+    mod->kol_tree=kol_tree;
+    mod->max_glub=max_glub;
+    mod->min_split=min_split;
+    mod->kol_priz_sub=kol_priz_sub;
+    mod->kol_class=0;
+    mod->kol_priz=0;
+    mod->trees= calloc(kol_tree,sizeof(Tree*));
+    return model;
+}
--- beging.h
+++ beging.h
@@ -1,1 +1,19 @@
+#ifndef BEGING_H
+#define BEGING_H
 
+#include "tree.h"
+
+typedef struct(
+    Tree **trees;
+    int kol_tree;
+    int max_glub;
+    int min_split;
+    int kol_priz_sub;
+    int kol_class;
+    int kol_priz;
+)BegModel;
+
+BegModel* create_beg_model(int kol_tree, int max_glub,int min_split,int kol_priz_sub);
+void freeModel(BegModel* mod);
+
+#endif
